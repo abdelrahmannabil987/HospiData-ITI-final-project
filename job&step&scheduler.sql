9b3afd1a3fe577d1b2739d66ae53efd08ee14d30
abdelrahmannabil987 <96392434+abdelrahmannabil987@users.noreply.github.com>
2025-12-16 13:52:16 +0200

    weekly_job

---
+++ job&step&scheduler.sql
@@ -0,0 +1,36 @@
+﻿USE msdb;
+GO
+
+EXEC sp_add_job
+    @job_name = N'Hospital_Weekly_Snapshot',
+    @enabled = 1,
+    @description = N'Weekly snapshot of hospital transactional tables (Visits, Admissions, Bills, etc.)';
+GO
+
+EXEC sp_add_jobstep
+    @job_name = N'Hospital_Weekly_Snapshot',
+    @step_name = N'Run dbo.RunWeeklySnapshot',
+    @subsystem = N'TSQL',
+    @database_name = N'Hospital_Salam',
+    @command = N'EXEC dbo.RunWeeklySnapshot;',
+    @on_success_action = 1,   -- Quit with success
+    @on_fail_action    = 2;   -- Quit with failure
+GO
+EXEC sp_add_schedule
+    @schedule_name = N'Weekly_Monday_1AM',
+    @freq_type = 8,           -- Weekly
+    @freq_interval = 2,       -- Monday (1=Sunday, 2=Monday, 4=Tuesday, ... bitmask)
+    @freq_recurrence_factor = 1, 
+    @active_start_time = 010000;  -- 01:00 AM
+GO
+
+
+EXEC sp_attach_schedule
+    @job_name = N'Hospital_Weekly_Snapshot',
+    @schedule_name = N'Weekly_Monday_1AM';
+GO
+
+EXEC sp_add_jobserver
+    @job_name = N'Hospital_Weekly_Snapshot',
+    @server_name = N'(local)';
+GO
